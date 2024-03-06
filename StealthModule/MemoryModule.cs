6d0248c61f5b48c65c6dde6b2fe14d159264a68c
eric0210 <77634181+hsheric0210@users.noreply.github.com>
2024-03-07 00:55:03 +0900

    Extra warnings for EraseHeaders()

--- StealthModule/MemoryModule.cs
+++ StealthModule/MemoryModule.cs
@@ -103,22 +103,34 @@ namespace StealthModule
         }
 
         /// <summary>
-        /// Overwrite the PE headers in the memory with random bytes to prevent getting memory dumped.
+        /// <para>Overwrite the PE headers in the memory with random bytes to prevent getting memory dumped.</para>
+        /// 
+        /// <para>
         /// Be careful! After this job done, you can no longer use the functions that access the PE header.
         /// For example, you can't resolve exports by calling 'GetExport' after erasing the PE header. (It will create errors)
+        /// </para>
+        /// 
+        /// Also, this may have other unintentional side effects such as:
+        /// <list type="bullet">
+        /// <item>Unable to use SEH(__try, __except, __finally) because the Exception data directory from the header is erased</item>
+        /// <item>Unable to TLS(Thread-local Storage) because the TLS data directory from the header is erased</item>
+        /// <item>Unable to access DLL resources because the Resources data directory from the header is erased</item>
+        /// </list>
+        /// 
+        /// See https://0xrick.github.io/win-internals/pe5/ for more header information
         /// </summary>
         /// <param name="random"></param>
         public void EraseHeaders(Random random = null)
         {
             // https://github.com/LordNoteworthy/al-khaser/blob/master/al-khaser/AntiDump/ErasePEHeaderFromMemory.cpp
 
-            const int headerSize = 0x1000;
+            const int headerSize = 0x1000; // '.text' section starts from 0x1000; all before that can be considered as header data.
             random = random ?? new Random();
 
             if (!NativeMethods.VirtualProtect(moduleBase, headerSize, MemoryProtection.READWRITE, out var oldProtection))
                 return; // Failed to unprotect
 
-            for (var i = 0; i < 0x1000; i++)
+            for (var i = 0; i < headerSize; i++)
                 Marshal.WriteByte(moduleBase, i, (byte)random.Next(0xff));
 
             NativeMethods.VirtualProtect(moduleBase, headerSize, oldProtection, out _); // Revert protection to attempt perfect crime
