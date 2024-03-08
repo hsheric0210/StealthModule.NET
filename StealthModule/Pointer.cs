055c7fa4473e89bf469acf62717a176e4e949627
eric0210 <77634181+hsheric0210@users.noreply.github.com>
2024-03-08 22:39:42 +0900

    Rewrote Pointer.cs
    Revert back the backing instance to IntPtr (was UIntPtr)

--- StealthModule/Pointer.cs
+++ StealthModule/Pointer.cs
@@ -8,33 +8,33 @@ namespace StealthModule
     /// </summary>
     public readonly struct Pointer
     {
-        public static readonly Pointer Zero = new Pointer(UIntPtr.Zero);
+        public static readonly Pointer Zero = new Pointer(IntPtr.Zero);
 
         public static int Size => IntPtr.Size;
 
         public static bool Is64Bit => Size == 8;
 
-        private readonly UIntPtr value;
+        private readonly IntPtr value;
 
-        private Pointer(UIntPtr value) => this.value = value;
+        private Pointer(IntPtr value) => this.value = value;
 
         // (Auto-)Boxing functions (object -> Pointer)
 
-        public static implicit operator Pointer(IntPtr value) => new Pointer((UIntPtr)unchecked((ulong)value.ToInt64()));
-        public static implicit operator Pointer(UIntPtr value) => new Pointer(value);
-        public static implicit operator Pointer(long value) => new Pointer((UIntPtr)unchecked((ulong)value));
-        public static implicit operator Pointer(ulong value) => new Pointer((UIntPtr)value);
-        public static implicit operator Pointer(int value) => new Pointer((UIntPtr)unchecked((uint)value));
-        public static implicit operator Pointer(uint value) => new Pointer((UIntPtr)value);
+        public static implicit operator Pointer(IntPtr value) => new Pointer(value);
+        public static implicit operator Pointer(UIntPtr value) => new Pointer((IntPtr)unchecked((long)value.ToUInt64()));
+        public static implicit operator Pointer(long value) => new Pointer((IntPtr)value);
+        public static implicit operator Pointer(ulong value) => new Pointer((IntPtr)unchecked((long)value));
+        public static implicit operator Pointer(int value) => new Pointer((IntPtr)value);
+        public static implicit operator Pointer(uint value) => new Pointer((IntPtr)value); // auto cast: uint -> long
 
         // (Auto-)Unboxing functions (Pointer -> object)
 
-        public static implicit operator IntPtr(Pointer value) => new IntPtr(unchecked((long)value.value));
-        public static implicit operator UIntPtr(Pointer value) => value.value;
-        public static explicit operator int(Pointer value) => unchecked((int)value.value.ToUInt32());
-        public static explicit operator uint(Pointer value) => value.value.ToUInt32();
-        public static explicit operator long(Pointer value) => unchecked((long)value.value.ToUInt64());
-        public static explicit operator ulong(Pointer value) => value.value.ToUInt64();
+        public static implicit operator IntPtr(Pointer value) => value.value;
+        public static implicit operator UIntPtr(Pointer value) => new UIntPtr(unchecked((ulong)value.value.ToInt64()));
+        public static explicit operator int(Pointer value) => value.value.ToInt32();
+        public static explicit operator uint(Pointer value) => unchecked((uint)value.value.ToInt32());
+        public static explicit operator long(Pointer value) => value.value.ToInt64();
+        public static explicit operator ulong(Pointer value) => unchecked((ulong)value.value.ToInt64());
 
         // Direct read/write
 
@@ -47,28 +47,20 @@ namespace StealthModule
         // Pointer arithmetics
 
         public static Pointer operator +(Pointer value, Pointer offset) => Is64Bit ? (Pointer)((ulong)value + (ulong)offset) : (Pointer)((uint)value + (uint)offset);
-        public static Pointer operator +(Pointer value, IntPtr offset) => Is64Bit ? (Pointer)((ulong)value + unchecked((ulong)offset.ToInt64())) : (Pointer)((uint)value + unchecked((uint)offset.ToInt32()));
-        public static Pointer operator +(Pointer value, UIntPtr offset) => Is64Bit ? (Pointer)((ulong)value + offset.ToUInt64()) : (Pointer)((uint)value + offset.ToUInt32());
-        public static Pointer operator +(Pointer value, int offset) => value + (uint)offset; // overflow may occur
-        public static Pointer operator +(Pointer value, uint offset) => value + (ulong)offset;
-        public static Pointer operator +(Pointer value, long offset) => value + (ulong)offset; // overflow may occur
-        public static Pointer operator +(Pointer value, ulong offset) => (Pointer)((ulong)value + offset);
+        public static Pointer operator +(Pointer value, IntPtr offset) => (IntPtr.Size == 8 ? (IntPtr)(value.value.ToInt64() + offset.ToInt64()) : (IntPtr)(value.value.ToInt32() + offset.ToInt32()));
+        public static Pointer operator +(Pointer value, UIntPtr offset) => (IntPtr.Size == 8 ? (IntPtr)(value.value.ToInt64() + unchecked((long)offset.ToUInt64())) : (IntPtr)(value.value.ToInt32() + unchecked((int)offset.ToUInt32())));
+        public static Pointer operator +(Pointer value, int offset) => (value.value.ToInt64() + offset);
+        public static Pointer operator +(Pointer value, uint offset) => (IntPtr.Size == 8 ? (IntPtr)(value.value.ToInt64() + unchecked((long)offset)) : (IntPtr)(value.value.ToInt32() + unchecked((int)offset)));
 
-        public static Pointer operator -(Pointer value, Pointer offset) => Is64Bit ? (Pointer)((ulong)value - (ulong)offset) : (Pointer)((uint)value - (uint)offset);
-        public static Pointer operator -(Pointer value, IntPtr offset) => Is64Bit ? (Pointer)((ulong)value - unchecked((ulong)offset.ToInt64())) : (Pointer)((uint)value - unchecked((uint)offset.ToInt32()));
-        public static Pointer operator -(Pointer value, UIntPtr offset) => Is64Bit ? (Pointer)((ulong)value - offset.ToUInt64()) : (Pointer)((uint)value - offset.ToUInt32());
-        public static Pointer operator -(Pointer value, int offset) => value - (uint)offset; // overflow may occur
-        public static Pointer operator -(Pointer value, uint offset) => value - (ulong)offset;
-        public static Pointer operator -(Pointer value, long offset) => value - (ulong)offset; // overflow may occur
-        public static Pointer operator -(Pointer value, ulong offset) => (Pointer)((ulong)value - offset);
+        public static Pointer operator -(Pointer value, IntPtr offset) => (IntPtr.Size == 8 ? (IntPtr)(value.value.ToInt64() - offset.ToInt64()) : (IntPtr)(value.value.ToInt32() - offset.ToInt32()));
 
         // Align
 
-        public static Pointer operator |(Pointer value, UIntPtr v) => Is64Bit ? (Pointer)((ulong)value | v.ToUInt64()) : (Pointer)((uint)value | v.ToUInt32());
-        public Pointer AlignDown(UIntPtr align) => (Pointer)(value.ToUInt64() & ~(align.ToUInt64() - 1));
-        public bool SpanBoundary(uint Size, int BoundaryBits) => value.ToUInt64() >> BoundaryBits < (value.ToUInt64() + Size) >> BoundaryBits;
+        public static Pointer operator |(Pointer value, UIntPtr v) => (IntPtr.Size == 8 ? (IntPtr)unchecked((long)(unchecked((ulong)value.value.ToInt64()) | v.ToUInt64())) : (IntPtr)unchecked((int)(unchecked((uint)value.value.ToInt32()) | v.ToUInt32())));
+        public Pointer AlignDown(UIntPtr align) => (Pointer)unchecked((long)(unchecked((ulong)value.ToInt64()) & ~(align.ToUInt64() - 1)));
+        public bool SpanBoundary(uint Size, int BoundaryBits) => ((unchecked((ulong)value.ToInt64()) >> BoundaryBits) < ((unchecked((ulong)(value.ToInt64())) + Size) >> BoundaryBits));
 
-        public bool IsInvalidHandle() => value == UIntPtr.Zero || value == (Is64Bit ? unchecked((UIntPtr)(ulong)-1L) : unchecked((UIntPtr)(uint)-1));
+        public bool IsInvalidHandle() => (value == IntPtr.Zero || value == (IntPtr.Size == 8 ? (IntPtr)(long)-1 : (IntPtr)(int)-1));
 
         // Comparison operator
 
