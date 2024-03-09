8c848fb7c524be2ee9eedfa8833f250334f21aa5
eric0210 <77634181+hsheric0210@users.noreply.github.com>
2024-03-09 11:18:46 +0900

    RunDll32 now supports optional parameter
    ... and also some additional warning mesages are added

--- Example/RunDll32/Program.cs
+++ Example/RunDll32/Program.cs
@@ -3,14 +3,18 @@ using System;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Runtime.ExceptionServices;
+using System.Linq;
 
 namespace RunDll32
 {
     internal class Program
     {
+        [DllImport("kernel32.dll", CallingConvention = CallingConvention.Winapi)]
+        private static extern IntPtr GetConsoleWindow();
+
         static void Main(string[] args)
         {
-            if (args.Length != 1)
+            if (args.Length < 1)
             {
                 PrintSyntax();
                 return;
@@ -31,13 +35,15 @@ namespace RunDll32
                 return;
             }
 
-            RunDLL(dllName, entryPoint);
+            var cmdLine = string.Join(" ", args.Skip(1));
+            RunDLL(dllName, entryPoint, cmdLine);
         }
 
-        private delegate void DllEntryPoint();
+        // https://blog.naver.com/ariesike/120133117676
+        private delegate void DllEntryPoint(IntPtr hWindow, IntPtr hInstance, [MarshalAs(UnmanagedType.LPWStr)] string lpszCmdLine, int nCmdShow);
 
         [HandleProcessCorruptedStateExceptions] // Catch AccessViolationException
-        private static void RunDLL(string dllName, string entryPointName)
+        private static void RunDLL(string dllName, string entryPointName, string cmdLine)
         {
             if (!File.Exists(dllName))
             {
@@ -61,8 +67,13 @@ namespace RunDll32
 
             try
             {
+                var hwnd = GetConsoleWindow();
                 var entry = Marshal.GetDelegateForFunctionPointer<DllEntryPoint>(entryPoint);
-                entry();
+
+                Console.WriteLine("[+] Console window handle is 0x" + (Pointer)hwnd);
+                Console.WriteLine($"[+] Command line is '{cmdLine}'");
+                entry(hwnd, module.ModuleBaseAddress, cmdLine, 1);// 1 = SW_NORMAL
+
                 Console.WriteLine("[+] The entry point call was successful.");
             }
             catch (Exception ex)
@@ -75,10 +86,11 @@ namespace RunDll32
         {
             Console.WriteLine("RunDll32 implemented in C# with StealthModule.NET");
             Console.WriteLine("This program will manual-map the specified dll, and then call the specified entry point function.");
-            Console.WriteLine("Warning: Optional parameters are unsupported.");
-            Console.WriteLine("Syntax: RunDll32.exe dll_name,EntryPoint");
+            Console.WriteLine("Some DLL would not work with this tool, especially for DLLs compiled with security features enabled (e.g. GS, SDL)");
+            Console.WriteLine("Syntax: RunDll32.exe dll_name,EntryPoint [optional parameters]");
             Console.WriteLine("\tdll_name - The DLL file to run");
             Console.WriteLine("\tEntryPoint - The entry point function name or ordinal (to use ordinal the '#' prefix must be appended)");
+            Console.WriteLine("\t[optional parameters] - Optional command line to pass through the DLL function.");
             Console.WriteLine("");
             Console.WriteLine("'Rundll32.exe abcd.dll,foobarbaz123' - Call the abcd.dll's 'foobarbaz123' exported function");
             Console.WriteLine("'Rundll32.exe efgh.dll,#1' - Call the efgh.dll's export function with ordinal 1");
