1c68ddeb34d7b260ee5e7ad194f4f3ee50d7f4b7
eric0210 <77634181+hsheric0210@users.noreply.github.com>
2024-03-10 11:01:03 +0900

    Refactor NativeMethods+Kernel32
    + Kernel32Delegates
    * Use GetExport<T>(string) instead of GetExport(string, type)

--- StealthModule/NativeMethods+Kernel32.cs
+++ StealthModule/NativeMethods+Kernel32.cs
@@ -1,25 +1,29 @@
 ﻿using System;
+using static StealthModule.NativeMethods.Kernel32Delegates;
 
 namespace StealthModule
 {
     internal partial class NativeMethods
     {
-        private delegate IntPtr DLoadLibrary(IntPtr lpFileName);
-        private delegate bool DFreeLibrary(IntPtr hModule);
-        private delegate IntPtr DVirtualAlloc(IntPtr lpAddress, UIntPtr dwSize, AllocationType flAllocationType, MemoryProtection flProtect);
-        private delegate bool DVirtualFree(IntPtr lpAddress, IntPtr dwSize, AllocationType dwFreeType);
-        private delegate bool DVirtualProtect(IntPtr lpAddress, IntPtr dwSize, MemoryProtection flNewProtect, out MemoryProtection lpflOldProtect);
-        private delegate void DGetNativeSystemInfo(out SystemInfo lpSystemInfo);
-        private delegate IntPtr DGetProcAddress(IntPtr hModule, IntPtr procName);
+        internal static class Kernel32Delegates
+        {
+            internal delegate IntPtr LoadLibrary(IntPtr lpFileName);
+            internal delegate bool FreeLibrary(IntPtr hModule);
+            internal delegate IntPtr VirtualAlloc(IntPtr lpAddress, UIntPtr dwSize, AllocationType flAllocationType, MemoryProtection flProtect);
+            internal delegate bool VirtualFree(IntPtr lpAddress, IntPtr dwSize, AllocationType dwFreeType);
+            internal delegate bool VirtualProtect(IntPtr lpAddress, IntPtr dwSize, MemoryProtection flNewProtect, out MemoryProtection lpflOldProtect);
+            internal delegate void GetNativeSystemInfo(out SystemInfo lpSystemInfo);
+            internal delegate IntPtr GetProcAddress(IntPtr hModule, IntPtr procName);
+        }
 
         static bool kernel32Initialized;
-        private static DLoadLibrary loadLibrary;
-        private static DFreeLibrary freeLibrary;
-        private static DVirtualAlloc virtualAlloc;
-        private static DVirtualFree virtualFree;
-        private static DVirtualProtect virtualProtect;
-        private static DGetNativeSystemInfo getNativeSystemInfo;
-        private static DGetProcAddress getProcAddress;
+        private static LoadLibrary loadLibrary;
+        private static FreeLibrary freeLibrary;
+        private static VirtualAlloc virtualAlloc;
+        private static VirtualFree virtualFree;
+        private static VirtualProtect virtualProtect;
+        private static GetNativeSystemInfo getNativeSystemInfo;
+        private static GetProcAddress getProcAddress;
 
         internal static Pointer LoadLibrary(Pointer lpFileName)
         {
@@ -84,13 +88,13 @@ namespace StealthModule
 
             var kernel32 = new ExportResolver("kernel32.dll");
             kernel32.CacheAllExports(); // A bit overkill, but it is much more efficient.
-            loadLibrary = (DLoadLibrary)kernel32.GetExport("LoadLibraryA", typeof(DLoadLibrary));
-            freeLibrary = (DFreeLibrary)kernel32.GetExport("FreeLibrary", typeof(DFreeLibrary));
-            virtualAlloc = (DVirtualAlloc)kernel32.GetExport("VirtualAlloc", typeof(DVirtualAlloc));
-            virtualFree = (DVirtualFree)kernel32.GetExport("VirtualFree", typeof(DVirtualFree));
-            virtualProtect = (DVirtualProtect)kernel32.GetExport("VirtualProtect", typeof(DVirtualProtect));
-            getNativeSystemInfo = (DGetNativeSystemInfo)kernel32.GetExport("GetNativeSystemInfo", typeof(DGetNativeSystemInfo));
-            getProcAddress = (DGetProcAddress)kernel32.GetExport("GetProcAddress", typeof(DGetProcAddress));
+            loadLibrary = kernel32.GetExport<LoadLibrary>("LoadLibraryA");
+            freeLibrary = kernel32.GetExport<FreeLibrary>("FreeLibrary");
+            virtualAlloc = kernel32.GetExport<VirtualAlloc>("VirtualAlloc");
+            virtualFree = kernel32.GetExport<VirtualFree>("VirtualFree");
+            virtualProtect = kernel32.GetExport<VirtualProtect>("VirtualProtect");
+            getNativeSystemInfo = kernel32.GetExport<GetNativeSystemInfo>("GetNativeSystemInfo");
+            getProcAddress = kernel32.GetExport<GetProcAddress>("GetProcAddress");
             kernel32Initialized = true;
         }
     }
