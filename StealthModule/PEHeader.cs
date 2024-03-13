0425c7408c2af8b35b65894c3e772ac5ae40c469
eric0210 <77634181+hsheric0210@users.noreply.github.com>
2024-03-14 02:02:25 +0900

    PEHeader will use "Pointer"-based pointer-arithmetics

--- StealthModule/PEHeader.cs
+++ StealthModule/PEHeader.cs
@@ -26,26 +26,26 @@ namespace StealthModule
         {
             BaseAddress = baseAddress;
 
-            var e_lfanew = (uint)Marshal.ReadInt32((IntPtr)((ulong)baseAddress + 0x3c));
-            NtHeadersSignature = (uint)Marshal.ReadInt32((IntPtr)((ulong)baseAddress + e_lfanew));
+            var e_lfanew = (uint)Marshal.ReadInt32(baseAddress + 0x3c);
+            NtHeadersSignature = (uint)Marshal.ReadInt32(baseAddress + e_lfanew);
 
             // Validate PE signature
-            if (NtHeadersSignature != 0x4550)
+            if (NtHeadersSignature != NativeMagics.IMAGE_NT_SIGNATURE)
                 throw new InvalidOperationException("Invalid PE signature.");
 
-            FileHeader = (ImageFileHeader)Marshal.PtrToStructure((IntPtr)((ulong)baseAddress + e_lfanew + 0x4), typeof(ImageFileHeader));
+            FileHeader = (ImageFileHeader)Marshal.PtrToStructure(baseAddress + e_lfanew + 0x4, typeof(ImageFileHeader));
 
-            var optHeaderAddress = (IntPtr)((ulong)baseAddress + e_lfanew + 0x18);
+            var optHeaderAddress = baseAddress + e_lfanew + 0x18;
             OptionalHeader = (ImageOptionalHeader)Marshal.PtrToStructure(optHeaderAddress, typeof(ImageOptionalHeader));
 
             var optHeaderMagic = (ushort)Marshal.ReadInt16(optHeaderAddress);
-            Is64Bit = optHeaderMagic == 0x020b;
+            Is64Bit = optHeaderMagic == (uint)NtOptionalHeaderMagic.Header64Magic;
 
             // Read sections
             var sections = new ImageSectionHeader[FileHeader.NumberOfSections];
             for (var i = 0; i < sections.Length; i++)
             {
-                var sectionHeaderAddress = (IntPtr)((ulong)optHeaderAddress + FileHeader.SizeOfOptionalHeader + (uint)(i * 0x28));
+                var sectionHeaderAddress = optHeaderAddress + FileHeader.SizeOfOptionalHeader + (uint)(i * 0x28);
                 sections[i] = (ImageSectionHeader)Marshal.PtrToStructure(sectionHeaderAddress, typeof(ImageSectionHeader));
             }
 
